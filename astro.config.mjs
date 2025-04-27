6860726ee87ad9014fffe3253522aae984532346
sankalp tank <sankalptank@sankalps-Air.localdomain>
2025-04-27 00:39:58 -0400

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default {
+    base: '/really-good-temporary-website/',  // Replace with your GitHub repository name
+};
+  
+
